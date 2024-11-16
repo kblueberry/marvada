40f0547f43a9a61bad457cfc2cd08bcbe384aa17
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-11-16 11:41:41 +0200

    added images to carousel

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -5,36 +5,52 @@ export const Slider = () => {
   return (
     <div style={{ width: "100%" }}>
       <Carousel>
-        <div style={{ minWidth: "500px" }}>
+        <div style={{ minWidth: "800px" }}>
           <Photo
-            src="/img/VarnaGL-announce-1.webp"
-            alt="Varna GL announce 1"
-            width={522}
-            height={694}
+            src="/img/VARNA_SHOOT_1_scaled.jpg"
+            alt="Varna shoot picture"
+            width={800}
+            height={1067}
           />
         </div>
-        <div style={{ minWidth: "500px" }}>
+        <div style={{ minWidth: "800px" }}>
           <Photo
-            src="/img/VarnaGL-announce-2.webp"
-            alt="Varna GL announce 2"
-            width={661}
-            height={530}
+            src="/img/VARNA_SHOOT_2_scaled.jpg"
+            alt="Varna shoot picture"
+            width={800}
+            height={683}
           />
         </div>
-        <div style={{ minWidth: "500px" }}>
+        <div style={{ minWidth: "800px" }}>
           <Photo
-            src="/img/VarnaGL-announce-1.webp"
-            alt="Varna GL announce 1"
-            width={522}
-            height={694}
+            src="/img/VARNA_SHOOT_3_scaled.jpg"
+            alt="Varna shoot picture"
+            width={800}
+            height={946}
           />
         </div>
-        <div style={{ minWidth: "600px" }}>
+        <div style={{ minWidth: "800px" }}>
           <Photo
-            src="/img/VarnaGL-announce-2.webp"
-            alt="Varna GL announce 2"
-            width={661}
-            height={530}
+            src="/img/VARNA_SHOOT_4_scaled.jpg"
+            alt="Varna shoot picture"
+            width={800}
+            height={1067}
+          />
+        </div>
+        <div style={{ minWidth: "800px" }}>
+          <Photo
+            src="/img/VARNA_SHOOT_5_scaled.jpg"
+            alt="Varna shoot picture"
+            width={800}
+            height={739}
+          />
+        </div>
+        <div style={{ minWidth: "920px" }}>
+          <Photo
+            src="/img/VARNA_SHOOT_6_scaled.jpg"
+            alt="Varna shoot picture"
+            width={920}
+            height={946}
           />
         </div>
       </Carousel>
