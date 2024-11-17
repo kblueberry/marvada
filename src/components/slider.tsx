06ac65369e78ba4a419a28ce32676d3533a9d8a3
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-11-17 20:22:50 +0200

    update carousel

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,59 +1,58 @@
 import { Photo } from "./ui-kit/photo";
 import { Carousel } from "nuka-carousel";
+import styles from "@/styles/Slider.module.scss";
 
-export const Slider = () => {
-  return (
-    <div style={{ width: "100%" }}>
-      <Carousel>
-        <div style={{ minWidth: "800px" }}>
-          <Photo
-            src="/img/VARNA_SHOOT_1_scaled.jpg"
-            alt="Varna shoot picture"
-            width={800}
-            height={1067}
-          />
-        </div>
-        <div style={{ minWidth: "800px" }}>
-          <Photo
-            src="/img/VARNA_SHOOT_2_scaled.jpg"
-            alt="Varna shoot picture"
-            width={800}
-            height={683}
-          />
-        </div>
-        <div style={{ minWidth: "800px" }}>
-          <Photo
-            src="/img/VARNA_SHOOT_3_scaled.jpg"
-            alt="Varna shoot picture"
-            width={800}
-            height={946}
-          />
-        </div>
-        <div style={{ minWidth: "800px" }}>
-          <Photo
-            src="/img/VARNA_SHOOT_4_scaled.jpg"
-            alt="Varna shoot picture"
-            width={800}
-            height={1067}
-          />
-        </div>
-        <div style={{ minWidth: "800px" }}>
-          <Photo
-            src="/img/VARNA_SHOOT_5_scaled.jpg"
-            alt="Varna shoot picture"
-            width={800}
-            height={739}
-          />
-        </div>
-        <div style={{ minWidth: "920px" }}>
-          <Photo
-            src="/img/VARNA_SHOOT_6_scaled.jpg"
-            alt="Varna shoot picture"
-            width={920}
-            height={946}
-          />
-        </div>
-      </Carousel>
-    </div>
-  );
-};
+export const Slider = () => (
+  <div className={styles.slider}>
+    <Carousel>
+      <div className={styles.slider__item}>
+        <Photo
+          src="/img/VARNA_SHOOT_1_scaled.jpg"
+          alt="Varna shoot picture"
+          width={800}
+          height={1067}
+        />
+      </div>
+      <div className={styles.slider__item}>
+        <Photo
+          src="/img/VARNA_SHOOT_2_scaled.jpg"
+          alt="Varna shoot picture"
+          width={800}
+          height={683}
+        />
+      </div>
+      <div className={styles.slider__item}>
+        <Photo
+          src="/img/VARNA_SHOOT_3_scaled.jpg"
+          alt="Varna shoot picture"
+          width={800}
+          height={946}
+        />
+      </div>
+      <div className={styles.slider__item}>
+        <Photo
+          src="/img/VARNA_SHOOT_4_scaled.jpg"
+          alt="Varna shoot picture"
+          width={800}
+          height={1067}
+        />
+      </div>
+      <div className={styles.slider__item}>
+        <Photo
+          src="/img/VARNA_SHOOT_5_scaled.jpg"
+          alt="Varna shoot picture"
+          width={800}
+          height={739}
+        />
+      </div>
+      <div className={styles["slider__item--last"]}>
+        <Photo
+          src="/img/VARNA_SHOOT_6_scaled.jpg"
+          alt="Varna shoot picture"
+          width={920}
+          height={946}
+        />
+      </div>
+    </Carousel>
+  </div>
+);
